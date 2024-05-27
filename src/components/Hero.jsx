6f5fa96ae239b5743ad7b8f921cdbc1f0f5bbda2
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-27 15:31:14 +0800

    profile update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
-import { logo } from "../assets";
 
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='' className='w-28 object-contain' />
+        <img src='/evil.png' alt='Evil Logo' className='w-10 object-contain' />
 
         <button
           type='button'
